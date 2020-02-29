27341b7a1301b04a5f3632242b951dfe2dd72eb9
azeghost <rikispanish123@gmail.com>
2020-02-29 19:03:26 +0100

    added first transformative notebook

--- training/traditional/transformative/AE.py
+++ training/traditional/transformative/AE.py
@@ -5,58 +5,14 @@ from graphs.basics.AE_graph import create_graph, encode_fn
 from training.traditional.autoencoders.autoencoder import autoencoder as basicAE
 
 class autoencoder(basicAE):
-    def __init__(
-            self,
-            name,
-            inputs_shape,
-            outputs_shape,
-            latent_dim,
-            variables_params,
-            filepath=None,
-            model_fn=create_graph
-    ):
 
-        basicAE.__init__(self,
-                         name=name,
-                         inputs_shape=inputs_shape,
-                         outputs_shape=outputs_shape,
-                         latent_dim=latent_dim,
-                         variables_params=variables_params,
-                         filepath=filepath,
-                         model_fn=model_fn)
+    def cast_batch(self, xt0, xt1):
+        xt0 = tf.cast(xt0, dtype=tf.float32)/self.input_scale
+        xt1 = tf.cast(xt1, dtype=tf.float32) / self.input_scale
 
-        self.encode_graph = encode_fn
-
-    def train_step(self, inputs, names):
-        try:
-            X = inputs[names[0]]
-        except:
-            X = inputs[0]
-        try:
-            Xt = inputs[names[1]]
-        except:
-            Xt = inputs[1]
-        with tf.GradientTape() as tape:
-            losses_dict = self.loss_functions()
-            for loss_name, loss_func in losses_dict.items():
-                losses_dict[loss_name] = loss_func(inputs=Xt, predictions=self.feedforwad(X))
-
-            losses = -sum([*losses_dict.values()])
-        gradients = tape.gradient(losses, self.get_trainables([*self.get_variables().values()]))
-        self.optimizer.apply_gradients(zip(gradients, self.get_trainables([*self.get_variables().values()])))
-        return losses
-
-    def evaluate_step(self, inputs, names):
-        try:
-            X = inputs[names[0]]
-        except:
-            X = inputs[0]
-        try:
-            Xt = inputs[names[1]]
-        except:
-            Xt = inputs[1]
-        losses_dict = self.loss_functions()
-        for loss_name, loss_func in losses_dict.items():
-            losses_dict[loss_name] = loss_func(inputs=Xt, predictions=self.feedforwad(X))
-        return losses_dict
-
+        return {
+                   'inference_inputs': xt0
+               }, \
+               {
+                   'x_logits': xt1
+               }
