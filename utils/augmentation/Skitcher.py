c446b980be47ce624442d3cf763ca700ff949c75
Khalid <kkahloots@gmail.com>
2020-03-10 23:18:41 +0100

    nw com

---
+++ utils/augmentation/Skitcher.py
@@ -0,0 +1,59 @@
+import random
+import cv2
+import numpy as np
+from Augmentor.Operations import Operation
+from PIL import Image
+import scipy.ndimage
+
+class Skitch(Operation):
+    """
+    This class is used to Color images .
+    """
+
+    def __init__(self, probability):
+        """
+        As the aspect ratio is always kept constant, only a
+        :attr:`scale_factor` is required for scaling the image.
+        :param probability: Controls the probability that the operation is
+         performed when it is invoked in the pipeline.
+        :param scale_factor: The factor by which to scale, where 1.5 would
+         result in an image scaled up by 150%.
+        :type probability: Float
+        :type scale_factor: Float
+        """
+        Operation.__init__(self, probability)
+
+    def perform_operation(self, images):
+        """
+        Scale the passed :attr:`images` by the factor specified during
+        instantiation, returning the scaled image.
+        :param images: The image to scale.
+        :type images: List containing PIL.Image object(s).
+        :return: The transformed image(s) as a list of object(s) of type
+         PIL.Image.
+        """
+
+        def do(image):
+            def grayscale(rgb):
+                return cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+            def dodge(front, back):
+                result = front * 255 / (255 - back)
+                result[result > 255] = 255
+                result[back == 255] = 255
+                return result / 255.0
+
+            gray_img = grayscale(image)
+            inverted_img = 255 - gray_img
+
+            blur_img = scipy.ndimage.filters.gaussian_filter(inverted_img, sigma=10)
+            final_img = dodge(blur_img, gray_img)
+
+            return 255-final_img
+
+        augmented_images = []
+
+        for image in images:
+            augmented_images.append(do(image))
+
+        return augmented_images
---
+++ utils/augmentation/augmentor.py
@@ -0,0 +1,32 @@
+import numpy as np
+from Augmentor.Operations import Operation, Skew, Distort, Rotate, Shear, Flip, Zoom, HistogramEqualisation
+from PIL import Image
+import cv2
+
+from utils.augmentation.Cloner import Clone
+from utils.augmentation.Colorizer import Colorize
+from utils.augmentation.Skitcher import Skitch
+
+import random
+def do_operation(opt, image):
+    image_cv = cv2.cvtColor((image * 255).astype(np.uint8), cv2.IMREAD_COLOR)[..., ::-1]
+    color = [0, 0, 0]
+    padding = 50
+    top, bottom = padding, padding
+    left, right = padding, padding
+    image_cv = cv2.copyMakeBorder(image_cv, top, bottom, left, right, cv2.BORDER_CONSTANT,
+                                  value=color)
+    return np.array(opt.perform_operation([Image.fromarray(image_cv)])[0])/255.0
+
+operations = {0: lambda: Skew(probability=1, skew_type="RANDOM", magnitude=1),
+              1: lambda: Distort(probability=1, grid_width=random.randint(1, 50), grid_height=random.randint(1, 50),
+                                 magnitude=5),
+              2: lambda: Rotate(probability=1, rotation=random.randint(1, 360)),
+              3: lambda: Shear(probability=1, max_shear_left=0, max_shear_right=random.randint(5, 15)) \
+                 if random.randint(0,1)==1 else Shear(probability=1, max_shear_left=random.randint(5, 15), max_shear_right=0),
+              4: lambda: Flip(probability=1, top_bottom_left_right="RANDOM"),
+              5: lambda: Zoom(probability=1, min_factor=random.randint(2, 10)/10, max_factor=random.randint(10, 12)/10),
+              6: lambda: Colorize(probability=1),
+              7: lambda: Skitch(probability=1),
+              8: lambda: Clone(probability=1)
+             }
