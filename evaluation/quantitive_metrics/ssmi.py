4b87bf9a0a7b0fd2182429e48886e54b9e48a443
Khalid <kkahloots@gmail.com>
2020-04-04 16:39:56 +0200

    augmentations

--- evaluation/quantitive_metrics/ssmi.py
+++ evaluation/quantitive_metrics/ssmi.py
@@ -3,5 +3,6 @@ def ssmi(inputs, x_logits):
     imageB = tf.sigmoid(x_logits)
     imageA  = inputs
 
-    ssmi = tf.reduce_mean(tf.image.ssim(imageA, imageB, max_val=1.0))
+    ssmi = 1-tf.reduce_mean(tf.image.ssim(imageA, imageB, max_val=1.0))
+    ssmi = 0.5*ssmi
     return ssmi
